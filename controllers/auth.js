274436493aff263124394ca08a60f2bd015a331b
T3sultan <60041747+T3sultan@users.noreply.github.com>
2020-10-13 21:25:42 -0700

    Add files via upload

---
+++ controllers/auth.js
@@ -0,0 +1,63 @@
+const User = require("../models/User");
+
+exports.register = async (req, res, next) => {
+  try {
+    //1.we take the data from client
+    const { name, email, password, bio } = req.body;
+    //validating if the user already exists
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return res
+        .status(400)
+        .json({ success: false, msg: "User already exists" });
+    }
+
+    //3.creating the user object and save it our mongo db
+    const user = await User.create({ name, email, password, bio });
+    //get the token
+    const token=user.getSignedJwtToken();
+
+    res.status(200).json({ success: true, user ,token});
+  } catch (error) {
+    res.status(500).json({ success: false, msg: "Internal error!" });
+    next(error);
+  }
+};
+
+exports.login = async (req, res, next) => {
+ 
+  try{
+    const {email, password} = req.body;
+    if(!email || !password){
+      return res
+      .status(400)
+      .json({ success: false, msg: "Please provide email and password" });
+
+    }
+   const user = await User.findOne({email: email}).select("password");
+  
+
+    if(!user){
+      return res
+      .status(400)
+      .json({ success: false, msg: "Invalid credentials" });
+
+    }
+    const isMatch = await user.matchPassword(password);
+
+    if(!isMatch){
+      return res
+      .status(400)
+      .json({ success: false, msg: "Invalid credentials"});
+    }
+    const token=user.getSignedJwtToken();
+    res.status(200).json({ success: true, user ,token});
+
+  }
+  catch(err){
+    res.status(500).json({ success: false, msg: "Internal error!" });
+    next(err);
+
+  }
+};
